56d5af4dd8d1315baf024cfc39d826674c375174
danpaslawsky@gmail.com <danpaslawsky@gmail.com>
2018-06-18 20:46:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // write your code below!
+var Name = Joe;
+var height = "74";
 
-var name = "Joe"
-var height = "74"
+console.log("Name:");
+console.log(name);
 
-console.log("Name:")
-console.log(name)
-
-console.log("Height:")
-console.log(height)
+console.log("Height:");
+console.log(height);
